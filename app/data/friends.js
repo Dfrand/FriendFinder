5c13a1a91f5bf8a5bbe4445ee0429d66b1368d89
Derek <d.frand@yahoo.com>
2018-03-02 10:14:59 -0600

    styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
 
     {
         "name": "Ahmed",
-        "photo": "https://metrouk2.files.wordpress.com/2018/02/692783492.jpg?w=748&h=420&crop=1",
+        "photo": "http://images2.fanpop.com/image/photos/10200000/cute-cat-witch-cute-kittens-10282827-400-400.jpg",
         "scores": [
             5,
             1,
@@ -18,9 +18,9 @@ var friends = [
     },
     {
         "name": "Derek",
-        "photo": "http://www.catster.com/wp-content/uploads/2017/08/Hairless-cat.jpg",
+        "photo": "https://pbs.twimg.com/profile_images/870051481486893056/f-tjeUzR_400x400.jpg",
         "scores": [
-            4,
+            5,
             3,
             1,
             3,
@@ -29,12 +29,12 @@ var friends = [
             1,
             3,
             5,
-            2
+            5
         ]
     },
     {
         "name": "Priscilla",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRejUuLVpp1bpqc4xfzmFKQaQGkKG_LY-PrneVoW_KeF8nK2Ia3",
+        "photo": "https://i.pinimg.com/736x/92/4b/05/924b059ea4d8511445b185d05909f46c--hipster-cat-hipster-beanie.jpg",
         "scores": [
             4,
             3,
@@ -66,7 +66,7 @@ var friends = [
     },
     {
         "name": "Sara",
-        "photo": "http://1.bp.blogspot.com/-l1r_ctHKtig/VP-8yjjz38I/AAAAAAAALSE/mIrHOlrZw-U/s1600/Cat.jpg",
+        "photo": "https://pbs.twimg.com/profile_images/948566007165194240/IMtRD-P6_400x400.jpg",
         "scores": [
             2,
             2,
@@ -82,7 +82,7 @@ var friends = [
     },
     {
         "name": "Beth",
-        "photo": "http://www.royalcanin.ca/~/media/Royal-Canin-Canada/Product-Categories/cat-adult-landing-hero.ashx",
+        "photo": "https://4h.extension.illinois.edu/sites/4h.extension.illinois.edu/files/styles/featured_detail_main_image/public/projects/189/feature_details/helpers-guide/cat-animal-17545_1920.jpg?itok=ujVnOS79",
         "scores": [
             4,
             2,
